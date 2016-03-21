fce28b83ea4d7e8d41f288d2684a361a532d7184
sty1ee <lsavage3@officeworks.com.au>
2016-03-21 12:09:43 +1100

    Push #6
    Changes to all pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
+
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn(500);
+		} else {
+			$('.scrollToTop').fadeOut(600);
+		}
+	});
 	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},1400);
+		return false;
+
+});
+	//Check to see if the window is top if not then display button
+		$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.goback').fadeIn(500);
+		} else {
+			$('.goback').fadeOut(600);
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.goback').click(function(){
+		$history.back();
+		return false;
+	
+
+});
+
+	});
+
+
+
 
